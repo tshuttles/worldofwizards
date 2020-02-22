60a905736ce4fbdf6f95605545156a223ed911e1
tshuttles <tshuttlesworth@gmail.com>
2020-02-22 13:26:48 -0600

    add spell controller methods

--- app/controllers/api/v1/spells_controller.rb
+++ app/controllers/api/v1/spells_controller.rb
@@ -1,3 +1,15 @@
 class Api::V1::SpellsController < ApplicationController
-  
+
+  def create 
+    name = Faker::Movies::HarryPotter.spell 
+    spell = Spell.create(name: name, wizard_id: params[:wizard_id])
+    render json: spell
+  end 
+
+  def destroy
+    spell = Spell.find(params[:id])
+    spell.delete
+    render json: {spellID: spell.id}
+  end
+
 end
