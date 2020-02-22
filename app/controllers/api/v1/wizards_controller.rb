ff26a2d8a5f1ebeb7cb0b033f6673eea7819cfb7
tshuttles <tshuttlesworth@gmail.com>
2020-02-22 12:17:19 -0600

    add wizard controller methods

--- app/controllers/api/v1/wizards_controller.rb
+++ app/controllers/api/v1/wizards_controller.rb
@@ -1,3 +1,35 @@
 class Api::V1::WizardsController < ApplicationController
-  
+
+  def index 
+    wizards = Wizard.all 
+    render json: wizards, status: 200
+  end 
+
+  def show 
+    wizard = Wizard.find(params[:id])
+    render json: wizard, status: 200
+  end 
+
+  def create 
+    wizard = Wizard.build(wizard_params)
+    if wizard.save 
+      render json: wizard, status: 200
+    else 
+      ender json: {error: "Error. Could not create wizard", status: 500}, status: 500
+    end 
+    render json: wizard 
+  end 
+
+  def destroy
+    wizard = Wizard.find(params[:id])
+    wizard.delete
+    render json: {wizardID: wizard.id}
+  end
+
+  private 
+
+  def wizard_params
+    params.require(wizard).permit(:name)
+  end 
+
 end
